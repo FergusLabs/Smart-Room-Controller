9b9c303d6bf58fd7cbef77ed53dc0b9832625df4
unknown <cfergus@cnm.edu>
2021-10-29 12:29:41 -0600

    fritzing and more

--- sunRiseWithHueWemo/sunRiseWithHueWemo.ino
+++ sunRiseWithHueWemo/sunRiseWithHueWemo.ino
@@ -1,5 +1,5 @@
 /*
- * project: Sunrise w/neoPIX
+ * project: Sunrise w/hue
  * name:Casey
  */
 
@@ -10,18 +10,16 @@
 #include "TM1637.h"
 #include <Encoder.h>
 #include <OneButton.h>
-
-#include <Adafruit_NeoPixel.h>
-#include <colors.h>
-
-
-const int PIXELPIN = 17;
-const int PIXELCOUNT = 1;
+//#include <wemo.h>
+#include <SPI.h>
+#include <Ethernet.h>
+#include <mac.h>
+#include <hue.h>
 
 #define ON 1
 #define OFF 0
 
-#define SCREEN_WIDTH 128
+#define SCREEN_WIDTH 128    //constants for OLED
 #define SCREEN_HEIGHT 64
 #define SCREEN_ADDRESS 0x3C
 #define BME_ADDRESS 0x76
@@ -29,11 +27,10 @@ const int PIXELCOUNT = 1;
 #define CLK 0 //pins for 4 digit display
 #define DIO 1
 
-const int pinA = 5, pinB = 6,buttonPin = 16;
+const int pinA = 5, pinB = 6,buttonPin = 16;    //pins for encoder(manual operation)
 
 OneButton encButton (buttonPin, INPUT_PULLUP);
 Encoder myEnc(pinA, pinB);
-Adafruit_NeoPixel pixel(PIXELCOUNT, PIXELPIN, NEO_GRB + NEO_KHZ800);
 TM1637 tm1637(CLK,DIO);
 Adafruit_SSD1306 display(SCREEN_WIDTH,SCREEN_HEIGHT, &Wire);
 time_t getTeensy3Time() {
@@ -42,19 +39,18 @@ time_t getTeensy3Time() {
 
 const int pirPin = 4,tonePin = 23;
 const int toneFreq = 528;
-bool motionDetect, mode, audioOn;
+bool motionDetect, mode, audioOn, sunRiseOn;
 int encPstn;  //encoder position
+int hueSelect = 3;
 
-unsigned long currentTime, lastSecond, sunRiseTimer;
+unsigned long currentTime, lastSecond, sunRiseTimer, sunRiseElapsed;
 
-int alarmHours = 12; 
-int alarmMinutes = 25;
+int alarmHours = 9; 
+int alarmMinutes = 41;
 
 int8_t TimeDisp[] = {0x00,0x00,0x00,0x00};
 unsigned char ClockPoint = 1;
 
-bool sunRiseOn, prevSunState;
-unsigned long sunRiseElapsed;
 int beginSunRiseMinutes, sunRiseHour,sunRiseMinutes;
 int earlyLUX, lateLUX, manLUX;
 
@@ -78,14 +74,15 @@ void setup() {
     Serial.printf("SSD1306 allocation failed");
     for(;;);
   }
+  Ethernet.begin(mac);
+  delay(200);          //ensure Serial Monitor is up and running           
+  printIP();
+  Serial.printf("LinkStatus: %i  \n",Ethernet.linkStatus());
 
   display.display();
   display.clearDisplay();
   display.display();
 
-  pixel.begin();
-  pixel.clear();
-  pixel.show();
   
   sunRiseOn = false;
   mode = false;
@@ -125,9 +122,7 @@ void manualMode (void) {
       myEnc.write(0);
     }
   manLUX = map(encPstn,0,96,0,255);
-  pixel.setPixelColor(0, yellow);
-  pixel.setBrightness(manLUX);
-  pixel.show();  
+  setHue(hueSelect, true, HueYellow, manLUX,0);  
   }
   if (!mode) {
     sunRiseLite();
@@ -158,16 +153,12 @@ void sunRiseLite (void) {
   if (sunRiseOn) { 
      sunRiseElapsed = ((currentTime)-(sunRiseTimer));
      if (sunRiseElapsed < 420000) {
-        earlyLUX = map(sunRiseElapsed,0,420000,1,35);
-        pixel.setPixelColor(0, blue);
-        pixel.setBrightness(earlyLUX);
-        pixel.show();
+        earlyLUX = map(sunRiseElapsed,0,420000,1,55);
+        setHue(hueSelect, true, HueViolet, earlyLUX, 255);
      }
      if (sunRiseElapsed > 420000) {
-        lateLUX = map(sunRiseElapsed,420000,600000,1,255);
-        pixel.setPixelColor(0, yellow);
-        pixel.setBrightness(lateLUX);
-        pixel.show();
+        lateLUX = map(sunRiseElapsed,420000,600000,10,255);
+        setHue(hueSelect, true, HueYellow, lateLUX, 0);
      }
   }
 }
@@ -227,7 +218,7 @@ void timeUpdate(void) { //time display to 4_Digit_Display
   tm1637.display(TimeDisp); 
 }
 
-#define TIME_HEADER  "T"   // Header tag for serial time sync message
+#define TIME_HEADER  "T"   // function to get time from PC
 unsigned long processSyncMessage() {
   unsigned long pctime = 0L;
   const unsigned long DEFAULT_TIME = 1357041600; // Jan 1 2013 
@@ -239,4 +230,12 @@ unsigned long processSyncMessage() {
      }
   }
   return pctime;
+}
+
+void printIP() {    //function to get IP address
+  Serial.printf("My IP address: ");
+  for (byte thisByte = 0; thisByte < 3; thisByte++) {
+    Serial.printf("%i.",Ethernet.localIP()[thisByte]);
+  }
+  Serial.printf("%i\n",Ethernet.localIP()[3]);
 }
